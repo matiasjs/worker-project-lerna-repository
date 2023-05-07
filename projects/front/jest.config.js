54109c56e7c448ba8aba7abc6e79f44f0d8d9bc3
Matias Ringa <ringa.matias@gmail.com>
2023-05-07 22:48:25 +0200

    update action v4

--- projects/front/jest.config.js
+++ projects/front/jest.config.js
@@ -11,7 +11,6 @@ const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
-    "^@/redux/(.*)$": "<rootDir>/src/redux/$1",
   },
 };
 
