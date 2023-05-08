46c919cd08296fd68f00f3c858c5707c2ead95e6
Lemongnz <lemongnz@gmail.com>
2023-05-08 12:30:50 -0300

    add fonts

--- projects/front/src/pages/index.page.tsx
+++ projects/front/src/pages/index.page.tsx
@@ -1,5 +1,18 @@
+import Head from "next/head";
 import LoginPage from "./login/index.page";
 
 export default function Home() {
-  return <LoginPage />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <LoginPage />;
+    </>
+  );
 }
